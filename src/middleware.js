581cb16bea11b0c5b468e2588ac04337390d6a61
harshraj.sam@gmail.com <harshraj.sam@gmail.com>
2023-12-21 16:28:56 +0530

    middleware

--- src/middleware.js
+++ src/middleware.js
@@ -9,13 +9,13 @@
 //     const cookiesHeader = req.headers.get('cookie') || '';
 //     const tokenMatch = cookiesHeader.match(/access_token=([^;]*)/);
 //     const token = tokenMatch ? tokenMatch[1] : '';
-   
+
 //     // const [cookies] = useCookies(['role']);
 
 //     // const roleCookie = cookies['role'];
 
 //     if (pbPath && token) {
-     
+
 //         return NextResponse.redirect(new URL(path, req.nextUrl));
 //     }
 //     if (!pbPath && !token) {
@@ -28,42 +28,41 @@
 //     matcher: ['/my-account', '/my-account/:path*', '/dashboard', '/dashboard/:path*'],
 // }
 
-
-
-
-
-
-import { NextResponse } from 'next/server'
-import { parse } from 'cookie';
+import { NextResponse } from "next/server";
+import { parse } from "cookie";
 export function middleware(req) {
-    const path = req.nextUrl.pathname;
-    const pbPath = path === "/login" || path === "/register" || path === "/reset-password" || path === "/forgot-password";
-    const patientPath = path === "/my-account";
-    const doctorPath = path === "/dashboard";
-    const cookies = parse(req.headers.get('cookie') || '');
-    const userRole = cookies['role'] || null;
-    
-    // Use the Cookies header from the request headers
-    const cookiesHeader = req.headers.get('cookie') || '';
-    const tokenMatch = cookiesHeader.match(/access_token=([^;]*)/);
-    const token = tokenMatch ? tokenMatch[1] : '';
+  const path = req.nextUrl.pathname;
+  const pbPath =
+    path === "/login" ||
+    path === "/register" ||
+    path === "/reset-password" ||
+    path === "/forgot-password";
+  const patientPath = path === "/my-account";
+  const doctorPath = path === "/dashboard";
+  const cookies = parse(req.headers.get("cookie") || "");
+  const userRole = cookies["role"] || null;
 
-    if (pbPath && token) {
-        return NextResponse.redirect(new URL(path, req.nextUrl));
-    }
-    if (!pbPath && !token) {
-        return NextResponse.redirect(new URL('/login', req.nextUrl));
-    }
-    console.log("path is ", path);
-        // if(patientPath && userRole != 'Patient'){
-        //     return NextResponse.redirect(new URL("/dashboard", req.nextUrl));
-        // }
-        if(doctorPath && userRole != 'Doctor'){
-            return NextResponse.redirect(new URL("/my-account", req.nextUrl));
-        }
+  console.log("Role", userRole);
+
+  // Use the Cookies header from the request headers
+  const cookiesHeader = req.headers.get("cookie") || "";
+  const tokenMatch = cookiesHeader.match(/access_token=([^;]*)/);
+  const token = tokenMatch ? tokenMatch[1] : "";
+
+  if (pbPath && token) {
+    return NextResponse.redirect(new URL(path, req.nextUrl));
+  }
+  if (!pbPath && !token) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }  
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/my-account', '/my-account/:path*', '/dashboard', '/dashboard/:path*'],
-}
+  matcher: [
+    "/my-account",
+    "/my-account/:path*",
+    "/dashboard",
+    "/dashboard/:path*",
+  ],
+};
